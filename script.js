aab15d9c147fc3d2aaa2a3e12dc3fa3a106e6270
Tim Jackson <timjacksonm@gmail.com>
2021-02-11 13:22:45 -0600

    re arranged how computerPlay() was being used. in 1 player mode when selecting computer to play first it now works.

--- script.js
+++ script.js
@@ -38,106 +38,79 @@ const displayController = (function() {
 
     if(turnSymbol == 'X') {
       switch (boxNum) {
-        case 'Box1':
+        case 'Box0':
           boxSelector[0].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-          boxSelector[compBoxNum].appendChild(O);
-          }
+          gameFlow.checkGameOver();
+          break;
+        case 'Box1':
+          boxSelector[1].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box2':
-          boxSelector[1].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[2].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box3':
-          boxSelector[2].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[3].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box4':
-          boxSelector[3].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[4].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box5':
-          boxSelector[4].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[5].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box6':
-          boxSelector[5].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[6].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box7':
-          boxSelector[6].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
+          boxSelector[7].appendChild(X);
           gameFlow.checkGameOver();
           break;
         case 'Box8':
-          boxSelector[7].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
-          gameFlow.checkGameOver();
-          break;
-        case 'Box9':
           boxSelector[8].appendChild(X);
-          if (playerTwo.name == 'Computer' && gameBoardScore.includes("")) {
-            boxSelector[compBoxNum].appendChild(O);
-            }
           gameFlow.checkGameOver();
           break;
         default:
           }
     }else if(turnSymbol == 'O') {
       switch (boxNum) {
-        case 'Box1':
+        case 'Box0':
           boxSelector[0].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box2':
+        case 'Box1':
           boxSelector[1].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box3':
+        case 'Box2':
           boxSelector[2].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box4':
+        case 'Box3':
           boxSelector[3].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box5':
+        case 'Box4':
           boxSelector[4].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box6':
+        case 'Box5':
           boxSelector[5].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box7':
+        case 'Box6':
           boxSelector[6].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box8':
+        case 'Box7':
           boxSelector[7].appendChild(O);
           gameFlow.checkGameOver();
           break;
-        case 'Box9':
+        case 'Box8':
           boxSelector[8].appendChild(O);
           gameFlow.checkGameOver();
           break;
@@ -166,51 +139,51 @@ const displayController = (function() {
 
   switch (position) {
     case '012':
-      Box1.setAttribute('class', 'topBorder leftBorder winBackground');
-      Box2.setAttribute('class', 'topBorder winBackground');
-      Box3.setAttribute('class', 'topBorder rightBorder winBackground');
+      Box0.setAttribute('class', 'topBorder leftBorder winBackground');
+      Box1.setAttribute('class', 'topBorder winBackground');
+      Box2.setAttribute('class', 'topBorder rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '345':
-      Box4.setAttribute('class', 'leftBorder winBackground');
-      Box5.setAttribute('class', 'winBackground');
-      Box6.setAttribute('class', 'rightBorder winBackground');
+      Box3.setAttribute('class', 'leftBorder winBackground');
+      Box4.setAttribute('class', 'winBackground');
+      Box5.setAttribute('class', 'rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '678':
-      Box7.setAttribute('class', 'leftBorder botBorder winBackground');
-      Box8.setAttribute('class', 'botBorder winBackground');
-      Box9.setAttribute('class', 'botBorder rightBorder winBackground');
+      Box6.setAttribute('class', 'leftBorder botBorder winBackground');
+      Box7.setAttribute('class', 'botBorder winBackground');
+      Box8.setAttribute('class', 'botBorder rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '036':
-      Box1.setAttribute('class', 'topBorder leftBorder winBackground');
-      Box4.setAttribute('class', 'leftBorder winBackground');
-      Box7.setAttribute('class', 'leftBorder botBorder winBackground');
+      Box0.setAttribute('class', 'topBorder leftBorder winBackground');
+      Box3.setAttribute('class', 'leftBorder winBackground');
+      Box6.setAttribute('class', 'leftBorder botBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '147':
-      Box2.setAttribute('class', 'topBorder winBackground');
-      Box5.setAttribute('class', 'winBackground');
-      Box8.setAttribute('class', 'botBorder winBackground');
+      Box1.setAttribute('class', 'topBorder winBackground');
+      Box4.setAttribute('class', 'winBackground');
+      Box7.setAttribute('class', 'botBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '258':
-      Box3.setAttribute('class', 'topBorder rightBorder winBackground');
-      Box6.setAttribute('class', 'rightBorder winBackground');
-      Box9.setAttribute('class', 'botBorder rightBorder winBackground');
+      Box2.setAttribute('class', 'topBorder rightBorder winBackground');
+      Box5.setAttribute('class', 'rightBorder winBackground');
+      Box8.setAttribute('class', 'botBorder rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '048':
-      Box1.setAttribute('class', 'topBorder leftBorder winBackground');
-      Box5.setAttribute('class', 'winBackground');
-      Box9.setAttribute('class', 'botBorder rightBorder winBackground');
+      Box0.setAttribute('class', 'topBorder leftBorder winBackground');
+      Box4.setAttribute('class', 'winBackground');
+      Box8.setAttribute('class', 'botBorder rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     case '642':
-      Box7.setAttribute('class', 'leftBorder botBorder winBackground');
-      Box5.setAttribute('class', 'winBackground');
-      Box3.setAttribute('class', 'topBorder rightBorder winBackground');
+      Box6.setAttribute('class', 'leftBorder botBorder winBackground');
+      Box4.setAttribute('class', 'winBackground');
+      Box2.setAttribute('class', 'topBorder rightBorder winBackground');
       _selectNav.style.backgroundColor = color;
       break;
     default:
@@ -407,58 +380,59 @@ const gameBoard = (function() {
   let _boxNum = ''; //Last box that was clicked on game screen.
 
 //below are game screen box event listeners that execute once when clicked.
-  let _Box1ClickEvent = (e) => {
+  let _Box0ClickEvent = (e) => {
      _boxNum = e.target.attributes[0].value;
      gameFlow.thePlayChoice(_boxNum, '0');
-     boxSelector[0].removeEventListener('click', _Box1ClickEvent);
+     boxSelector[0].removeEventListener('click', _Box0ClickEvent);
+  };
+  let _Box1ClickEvent = (e) => {
+    _boxNum = e.target.attributes[0].value;
+    gameFlow.thePlayChoice(_boxNum, '1');
+    boxSelector[1].removeEventListener('click', _Box1ClickEvent);
   };
   let _Box2ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '1');
-    boxSelector[1].removeEventListener('click', _Box2ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '2');
+    boxSelector[2].removeEventListener('click', _Box2ClickEvent);
   };
   let _Box3ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '2');
-    boxSelector[2].removeEventListener('click', _Box3ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '3');
+    boxSelector[3].removeEventListener('click', _Box3ClickEvent);
   };
   let _Box4ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '3');
-    boxSelector[3].removeEventListener('click', _Box4ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '4');
+    boxSelector[4].removeEventListener('click', _Box4ClickEvent);
   };
   let _Box5ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '4');
-    boxSelector[4].removeEventListener('click', _Box5ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '5');
+    boxSelector[5].removeEventListener('click', _Box5ClickEvent);
   };
   let _Box6ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '5');
-    boxSelector[5].removeEventListener('click', _Box6ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '6');
+    boxSelector[6].removeEventListener('click', _Box6ClickEvent);
   };
   let _Box7ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '6');
-    boxSelector[6].removeEventListener('click', _Box7ClickEvent);
+    gameFlow.thePlayChoice(_boxNum, '7');
+    boxSelector[7].removeEventListener('click', _Box7ClickEvent);
   };
   let _Box8ClickEvent = (e) => {
     _boxNum = e.target.attributes[0].value;
-    gameFlow.thePlayChoice(_boxNum, '7');
-    boxSelector[7].removeEventListener('click', _Box8ClickEvent);
-  };
-  let _Box9ClickEvent = (e) => {
-    _boxNum = e.target.attributes[0].value;
     gameFlow.thePlayChoice(_boxNum, '8');
-    boxSelector[8].removeEventListener('click', _Box9ClickEvent);
+    boxSelector[8].removeEventListener('click', _Box8ClickEvent);
   };
 
   function createBoard() {
-    for (let i = 1; i < 10; i++) {
+    for (let i = 0; i < 9; i++) {
       const createBoard = document.createElement('div');
       createBoard.setAttribute('id', `Box${i}`);
       _selectMain.appendChild(createBoard);
     };
+    const Box0 = document.getElementById('Box0');
     const Box1 = document.getElementById('Box1');
     const Box2 = document.getElementById('Box2');
     const Box3 = document.getElementById('Box3');
@@ -467,8 +441,7 @@ const gameBoard = (function() {
     const Box6 = document.getElementById('Box6');
     const Box7 = document.getElementById('Box7');
     const Box8 = document.getElementById('Box8');
-    const Box9 = document.getElementById('Box9');
-    boxSelector = [Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8, Box9];
+    boxSelector = [Box0, Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8];
     boxSelector[0].setAttribute('class', 'topBorder leftBorder fadeInBoard');
     boxSelector[1].setAttribute('class', 'topBorder fadeInBoard');
     boxSelector[2].setAttribute('class', 'topBorder rightBorder fadeInBoard');
@@ -483,31 +456,34 @@ const gameBoard = (function() {
   
     addOrRemoveEvents();
     displayController.menuPlayerNames();
+    if (playerTwo.name == 'Computer' && playerTwo.turn == 'First') {
+      gameFlow.computerPlay();
+    }
   };
   function addOrRemoveEvents() {
     switch (_gameBoardEventToggle) {
       case 'off': // used at beginning of game. adds to each Box on game board.
-        boxSelector[0].addEventListener('click', _Box1ClickEvent);
-        boxSelector[1].addEventListener('click', _Box2ClickEvent);
-        boxSelector[2].addEventListener('click', _Box3ClickEvent);
-        boxSelector[3].addEventListener('click', _Box4ClickEvent);
-        boxSelector[4].addEventListener('click', _Box5ClickEvent);
-        boxSelector[5].addEventListener('click', _Box6ClickEvent);
-        boxSelector[6].addEventListener('click', _Box7ClickEvent);
-        boxSelector[7].addEventListener('click', _Box8ClickEvent);
-        boxSelector[8].addEventListener('click', _Box9ClickEvent);
+        boxSelector[0].addEventListener('click', _Box0ClickEvent);
+        boxSelector[1].addEventListener('click', _Box1ClickEvent);
+        boxSelector[2].addEventListener('click', _Box2ClickEvent);
+        boxSelector[3].addEventListener('click', _Box3ClickEvent);
+        boxSelector[4].addEventListener('click', _Box4ClickEvent);
+        boxSelector[5].addEventListener('click', _Box5ClickEvent);
+        boxSelector[6].addEventListener('click', _Box6ClickEvent);
+        boxSelector[7].addEventListener('click', _Box7ClickEvent);
+        boxSelector[8].addEventListener('click', _Box8ClickEvent);
         _gameBoardEventToggle = 'on';
         break;
       case 'on': // used on win or loss screen - prevent further adding symbols to the game board.
-        boxSelector[0].removeEventListener('click', _Box1ClickEvent);
-        boxSelector[1].removeEventListener('click', _Box2ClickEvent);
-        boxSelector[2].removeEventListener('click', _Box3ClickEvent);
-        boxSelector[3].removeEventListener('click', _Box4ClickEvent);
-        boxSelector[4].removeEventListener('click', _Box5ClickEvent);
-        boxSelector[5].removeEventListener('click', _Box6ClickEvent);
-        boxSelector[6].removeEventListener('click', _Box7ClickEvent);
-        boxSelector[7].removeEventListener('click', _Box8ClickEvent);
-        boxSelector[8].removeEventListener('click', _Box9ClickEvent);
+        boxSelector[0].removeEventListener('click', _Box0ClickEvent);
+        boxSelector[1].removeEventListener('click', _Box1ClickEvent);
+        boxSelector[2].removeEventListener('click', _Box2ClickEvent);
+        boxSelector[3].removeEventListener('click', _Box3ClickEvent);
+        boxSelector[4].removeEventListener('click', _Box4ClickEvent);
+        boxSelector[5].removeEventListener('click', _Box5ClickEvent);
+        boxSelector[6].removeEventListener('click', _Box6ClickEvent);
+        boxSelector[7].removeEventListener('click', _Box7ClickEvent);
+        boxSelector[8].removeEventListener('click', _Box8ClickEvent);
         _gameBoardEventToggle = 'off';
         break;
       default:
@@ -536,6 +512,7 @@ const gameFlow = (function() {
     let fillByX = gameBoardScore.map(string => string.includes("O") || string.includes("X"))
     let indexListEmpty = [];
     let computerPlay = '';
+    let boxNum = '';
 
     function getRandomArbitrary(min, max) {
       return Math.random() * (max - min) + min;
@@ -548,10 +525,11 @@ const gameFlow = (function() {
       let num = parseInt(getRandomArbitrary(1,9).toFixed(0));
       if(indexListEmpty.includes(num)) {
         computerPlay = num;
+        boxNum = 'Box' + num;
         break;
       };
     };
-    return computerPlay;
+    thePlayChoice(boxNum, computerPlay);
   };
   function startGameEvent() {
     const startGameButton = document.getElementById('startGameButton');
@@ -613,21 +591,9 @@ const gameFlow = (function() {
     };
 };
   const thePlayChoice = (_boxNum, index) => {
-    if (playerTwo.name == 'Computer') {
-      turnSymbol = playerOne.symbol
-      gameBoardScore[index] = turnSymbol;
-      let compBoxNum = computerPlay();
-      gameBoardScore[compBoxNum] = 'O';
-      console.log(gameBoardScore);
-      displayController.placeSymbol(_boxNum,turnSymbol,compBoxNum);
-    }else {
     _checkTurn(playerOne.turn, playerTwo.turn);
     gameBoardScore[index] = turnSymbol;
-    let compBoxNum = computerPlay();
-    gameBoardScore[compBoxNum] = 'O';
-    console.log(gameBoardScore);
-    displayController.placeSymbol(_boxNum,turnSymbol,compBoxNum);
-    }
+    displayController.placeSymbol(_boxNum,turnSymbol);
   };
   function checkGameOver() {
       let symbol = '';
@@ -679,7 +645,7 @@ const gameFlow = (function() {
           position = '678';
           result = 'Lose';
           displayController.endGameDisplay(symbol, result, position);
-          gameBoard.addOrRemoveEvents();o
+          gameBoard.addOrRemoveEvents();
           return;
         }
       }else if(gameBoardScore[0]+gameBoardScore[3]+gameBoardScore[6] == "XXX" || gameBoardScore[0]+gameBoardScore[3]+gameBoardScore[6] == "OOO") {
@@ -763,6 +729,9 @@ const gameFlow = (function() {
           return;
         }
       }else if(gameBoardScore.includes("")){
+        if (playerTwo.name == 'Computer' && playerOne.symbol == turnSymbol) {
+          gameFlow.computerPlay();
+        }
         // console.log("Game stil ongoing");
         return;
       }else {
@@ -787,9 +756,9 @@ function _reset() {
   _selectNav.textContent = '';
   _selectNav.style.backgroundColor = '#3355dd';
   _selectMain.replaceChildren();
-  gameBoard.createBoard();
   gameBoardScore = ["", "", "", "", "", "", "", "", ""];
   turnSymbol = '';
+  gameBoard.createBoard();
 };
   return {
     startGameEvent: startGameEvent,
